556773a09210bcc2bdbca24289fa469e428b98e9
Nishant Hadole <nishant.hadole@scania.com>
2018-02-23 12:20:49 +0100

    Added Logback support for logging

---
+++ src/main/java/com/looklet/test/image/Application.java
@@ -0,0 +1,10 @@
+package com.looklet.test.image;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+    public static void main(String[] args) {
+        log.debug("Hello World!");
+    }
+}
